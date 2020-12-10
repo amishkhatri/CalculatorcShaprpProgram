4e2a223f16047884edd8b2472435b94a21ce9d5f
amish khatri <amish.khatri@gmail.com>
2020-12-10 14:34:14 +0530

    Add project files.

---
+++ UTConsoleProcessing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using calc.integration;
+using calc.operations;
+using calc.console;
+using calc.validation;
+
+
+namespace UTCalculatorProgram
+{
+   public class UTConsoleProcessing
+    {
+
+        [Fact]
+        public void ConsoleProces()
+        {
+
+
+
+        }
+
+
+    }
+}
---
+++ UTSampleProgram.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using calc.integration;
+using calc.console;
+
+
+namespace EntryPointProgram.UT.Program
+{
+    public class UTSampleProgram
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
